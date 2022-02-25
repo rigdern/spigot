a35bd96517766bd69cb8f74d719d8292840e3a7c
Adam Comella <rigdern@gmail.com>
2022-02-24 16:03:50 -0800

    Implement a function for executing the simulation
    The thermometer model appears to be working.

--- main.js
+++ main.js
@@ -1,39 +1,101 @@
 // Thermostat example (figure 15)
 
-[
-  {id: 'degrees Celsius', type: 'unit', name: 'degrees Celsius'},
+const spec = [
+  { id: 'degrees Celsius', type: 'unit', name: 'degrees Celsius' },
 
-  {id: 'room temperature', type: 'stock', name: 'room temperature', initialValue: 10, unit: 'degrees Celsius'},
+  // initialValue by scenario
+  //   - cold-room warming: 10 degrees C
+  //   - warm-room cooling: 18 degrees C
+  {
+    id: 'room temperature',
+    type: 'stock',
+    name: 'room temperature',
+    initialValue: 10,
+    unit: 'degrees Celsius'
+  },
 
-  {id: 'furnace', type: 'boundary'},
-  {id: 'heat from furnace', type: 'flow', name: 'heat from furnace', from: 'furnace', to: 'room temperature'},
+  {
+    id: 'furnace',
+    type: 'boundary',
+    unit: 'degrees Celsius'
+  },
 
-  {id: 'outside', type: 'boundary'},
-  {id: 'heat to outside', type: 'flow', name: 'heat to outside', from: 'room temperature', to: 'outside'},
+  {
+    id: 'heat from furnace',
+    type: 'flow',
+    name: 'heat from furnace',
+    from: 'furnace',
+    to: 'room temperature',
+    inConverters: [
+      'discrepancy between desired and actual room temperatures'
+    ],
+    logic: function (discrepancy) {
+      return Math.min(5, discrepancy)
+    }
+  },
 
-  {id: 'thermostat setting', type: 'parameter', name: 'thermostat setting', value: 18, unit: 'degrees Celsius'},
-  {id: 'outside temperature', type: 'parameter', name: 'outside temperature', value: 10, unit: 'degrees Celsius'}, // TODO: Value needs to vary over time
+  {
+    id: 'outside',
+    type: 'boundary',
+    unit: 'degrees Celsius'
+  },
+
+  {
+    id: 'heat to outside',
+    type: 'flow',
+    name: 'heat to outside',
+    from: 'room temperature',
+    to: 'outside',
+    inConverters: [
+      'discrepancy between inside and outside temperatures'
+    ],
+    logic: function (discrepancy) {
+      return 0.1 * discrepancy
+    }
+  },
 
   {
     id: 'discrepancy between desired and actual room temperatures',
-    type: 'rule',
+    type: 'converter',
     name: 'discrepancy between desired and actual room temperatures',
-    input: [
+    inputs: [
       'room temperature',
       'thermostat setting',
     ],
-    output: ['heat from furnace'],
+    logic: function (roomTemperature, thermostatSetting) {
+      return thermostatSetting - roomTemperature
+    }
   },
+
   {
     id: 'discrepancy between inside and outside temperatures',
-    type: 'rule',
+    type: 'converter',
     name: 'discrepancy between inside and outside temperatures',
-    input: [
+    inputs: [
       'room temperature',
       'outside temperature',
     ],
-    output: ['heat to outside'],
+    logic: function (roomTemperature, outsideTemperature) {
+      return roomTemperature - outsideTemperature
+    }
   },
+
+  {
+    id: 'thermostat setting',
+    type: 'parameter',
+    name: 'thermostat setting',
+    value: 18,
+    unit: 'degrees Celsius'
+  },
+  {
+    id: 'outside temperature',
+    type: 'parameter',
+    name: 'outside temperature',
+    value: 10,
+    unit: 'degrees Celsius'
+  }, // TODO: Value needs to vary over time
+
+  
 ]
 
 /*
@@ -53,3 +115,71 @@
   *   discrepancy between inside and outside temperature = room temperature - 10 degrees C ... for constant outside temperature (Figures 16-18);
   *     room temperature - 24-hour outside temp...for full day-and-night cycle (Figures 19 and 20)
 */
+
+function runStep(spec, state) {
+  const newState = {}
+
+  // Initialize state if necessary
+  if (state === undefined) {
+    state = {}
+    for (const stock of spec.filter(x => x.type === 'stock')) {
+      state[stock.id] = stock.initialValue
+    }
+  }
+  Object.assign(newState, state)
+
+  // Run all flows and update state
+  for (const flow of spec.filter(x => x.type === 'flow')) {
+    const flux = flow.logic.apply(undefined, flow.inConverters.map(x => resolveConverter(spec, state, x)))
+
+    if (lookup(spec, flow.to).type === 'stock') {
+      newState[flow.to] += flux
+    }
+    if (lookup(spec, flow.from).type === 'stock') {
+      newState[flow.from] -= flux
+    }
+  }
+
+  return newState
+}
+
+function resolveConverter(spec, state, converterId) {
+  const converter = lookup(spec, converterId)
+  return converter.logic.apply(undefined, converter.inputs.map(input => resolve(spec, state, input)))
+}
+
+function resolve(spec, state, id) {
+  const object = lookup(spec, id)
+  switch (object.type) {
+    case 'parameter':
+      return object.value
+    case 'converter': 
+      return resolveConverter(spec, state, object)
+    case 'stock':
+      return state[object.id]
+  }
+}
+
+function range(begin, end, inc) {
+  if (inc === undefined) inc = 1;
+  
+  const result = [];
+  for (let i = begin; i < end; i+= inc) {
+    result.push(i);
+  }
+  return result;
+}
+
+function lookup(spec, id) {
+  for (obj of spec) {
+    if (obj.id === id) {
+      return obj
+    }
+  }
+  throw new Error(`lookup: Failed to find object with id ${id} in spec given`)
+}
+
+let state = runStep(spec);
+console.table(range(0, 99).
+  reduce((statelog, _) => (statelog.push(runStep(spec, statelog[statelog.length - 1])), statelog), [])
+)
